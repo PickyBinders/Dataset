f551d364f3ee91f5f005f7b3068c742a672e88c6
Ninjani <janani.durairaj@gmail.com>
2024-01-10 13:54:15 +0100

    (feat): change scoring to work on divided PDB folders

--- src/run_foldseek.sh
+++ src/run_foldseek.sh
@@ -4,7 +4,6 @@ OUTPUT_DIR=$2
 PDB_LIST=$3
 OUTPUT_NAME=$4
 
-mkdir -p "$OUTPUT_DIR"
 PDB_FS_DIR="$OUTPUT_DIR/foldseek_pdb"
 if [ ! -f "$OUTPUT_DIR/foldseek_pdb"]; then
     foldseek createdb "$PDB_DIR" "$PDB_FS_DIR" --chain-name-mode 1
@@ -12,14 +11,21 @@ if [ ! -f "$OUTPUT_DIR/foldseek_pdb"]; then
 fi
 
 POCKET_FS_DIR="$OUTPUT_DIR/$OUTPUT_NAME"
-mkdir -p "$POCKET_FS_DIR"
 
-awk 'NR == FNR {f[$1] = $1; next} $2 in f {print $1}' "$PDB_LIST" "$PDB_FS_DIR".lookup > "$POCKET_FS_DIR/$OUTPUT_NAME".tsv
+awk 'NR == FNR {f[$1] = $1; next} $2 in f {print $1}' "$POCKET_FS_DIR/$OUTPUT_NAME".txt "$PDB_FS_DIR".lookup > "$POCKET_FS_DIR/$OUTPUT_NAME".tsv
 foldseek createsubdb "$POCKET_FS_DIR/$OUTPUT_NAME".tsv "$PDB_FS_DIR" "$POCKET_FS_DIR/$OUTPUT_NAME" --subdb-mode 1
 foldseek createsubdb "$POCKET_FS_DIR/$OUTPUT_NAME".tsv "$PDB_FS_DIR"_ss "$POCKET_FS_DIR/$OUTPUT_NAME"_ss --subdb-mode 1
 foldseek createsubdb "$POCKET_FS_DIR/$OUTPUT_NAME".tsv "$PDB_FS_DIR"_ca "$POCKET_FS_DIR/$OUTPUT_NAME"_ca --subdb-mode 1
 
-POCKET_FS_SEARCH_DIR="$POCKET_FS_DIR/search"
+for subset_file in $POCKET_FS_DIR/subset_files/*.txt; do
+    subset_name=$(basename "$subset_file" .txt)
+    awk 'NR == FNR {f[$1] = $1; next} $2 in f {print $1}' $subset_file "$PDB_FS_DIR".lookup > "$POCKET_FS_DIR/subset_files/$subset_name".tsv
+    foldseek createsubdb "$POCKET_FS_DIR/subset_files/$subset_name".tsv "$PDB_FS_DIR" "$POCKET_FS_DIR/subset_files/$subset_name" --subdb-mode 1
+    foldseek createsubdb "$POCKET_FS_DIR/subset_files/$subset_name".tsv "$PDB_FS_DIR"_ss "$POCKET_FS_DIR/subset_files/$subset_name"_ss --subdb-mode 1
+    foldseek createsubdb "$POCKET_FS_DIR/subset_files/$subset_name".tsv "$PDB_FS_DIR"_ca "$POCKET_FS_DIR/subset_files/$subset_name"_ca --subdb-mode 1
 
-foldseek search "$POCKET_FS_DIR/$OUTPUT_NAME" "$POCKET_FS_DIR/$OUTPUT_NAME" "$POCKET_FS_SEARCH_DIR" tmp -a -e 0.01 --max-seqs 1000 --sort-by-structure-bits 0
-foldseek convertalis "$POCKET_FS_DIR/$OUTPUT_NAME" "$POCKET_FS_DIR/$OUTPUT_NAME" "$POCKET_FS_SEARCH_DIR" "$POCKET_FS_DIR/aln.tsv" --format-mode 4 --format-output "query,target,lddt,fident,alnlen,qstart,qend,tstart,tend,evalue,bits,qcov,tcov,qaln,taln,lddtfull"
+    POCKET_FS_SEARCH_DIR_SUBSET="$POCKET_FS_DIR/subset_files/search_$subset_name"
+
+    foldseek search "$POCKET_FS_DIR/subset_files/$subset_name" "$POCKET_FS_DIR/$OUTPUT_NAME" "$POCKET_FS_SEARCH_DIR_SUBSET" tmp -a -e 0.01 --max-seqs 5000 --sort-by-structure-bits 0
+    foldseek convertalis "$POCKET_FS_DIR/subset_files/$subset_name" "$POCKET_FS_DIR/$OUTPUT_NAME" "$POCKET_FS_SEARCH_DIR_SUBSET" "$POCKET_FS_DIR/subset_files/aln_$subset_name.tsv" --format-mode 4 --format-output "query,target,qlen,lddt,fident,alnlen,qstart,qend,tstart,tend,evalue,bits,qcov,tcov,qaln,taln,lddtfull"
+done
