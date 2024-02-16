709eab6e6ee9b7ec92478997c5dec64aa2dc7efa
Ninjani <janani.durairaj@gmail.com>
2024-02-16 21:26:49 +0100

    parallelize graph parsing

--- src/graph_clustering.py
+++ src/graph_clustering.py
@@ -1,8 +1,8 @@
 import networkx as nx
 from tqdm import tqdm
 from pathlib import Path
-import numpy as np
 import json
+from multiprocessing import Pool
 
 def get_connected_components(graph, strong=True):
     if strong:
@@ -16,26 +16,34 @@ def get_connected_components(graph, strong=True):
     return key_to_component
 
 
-def label_protein_pocket_clusters(score_files, score_name, thresholds, strong=True):
+def get_edges(args):
+    score_file, score_name, threshold = args
+    edges = []
+    with open(score_file) as f:
+        for i, line in enumerate(f):
+            if not len(line):
+                continue
+            if i == 0:
+                columns = line.strip().split("\t")
+                continue
+            parts = dict(zip(columns, line.strip().split("\t")))
+            if parts[score_name] == "nan":
+                continue
+            score = float(parts[score_name])
+            if score < threshold:
+                continue
+            edges.append((parts["query_pocket"], parts["target_pocket"], score))
+    return edges
+
+def label_protein_pocket_clusters(score_dir, score_name, thresholds, strong=True, num_threads=8):
     thresholds = sorted(thresholds)
     if strong:
         graph = nx.DiGraph()
     else:
         graph = nx.Graph()
-    for score_file in tqdm(score_files):
-        with open(score_file) as f:
-            for i, line in enumerate(f):
-                if not len(line):
-                    continue
-                if i == 0:
-                    columns = line.strip().split("\t")
-                    continue
-                parts = line.strip().split("\t")
-                parts = dict(zip(columns, parts))
-                score = float(parts[score_name])
-                if score < thresholds[0] or np.isnan(score) or str(score) == "nan":
-                    continue
-                graph.add_edge(parts["query_pocket"], parts["target_pocket"], score=score)
+    with Pool(num_threads) as p:
+        for edge_list in tqdm(p.imap(get_edges, [(score_file, score_name, thresholds[0]) for score_file in score_dir.iterdir()]), total=1060):
+            graph.add_weighted_edges_from(edge_list, weight="score")
     key_to_component = {thresholds[0]: get_connected_components(graph, strong=strong)}
     for threshold in tqdm(thresholds[1:]):
         graph.remove_edges_from([e for e in graph.edges(data=True) if e[2]["score"] < threshold])
@@ -49,8 +57,8 @@ def label_df(pocket_df, cluster_dir):
         score_name, strong = cluster_file.stem.split("__")
         for key in key_to_component:
             col = f"{score_name}__{key}__{strong}__component"
-            pocket_df[col] = pocket_df["pocket_ID"].apply(lambda x: key_to_component[key].get(x, None))
-            pocket_df[f"{col}__not_found"] = pocket_df[col].apply(lambda x: x is None)
+            pocket_df[f"{col}__not_found"] = pocket_df["system_ID"].apply(lambda x: x not in key_to_component[key])
+            pocket_df[col] = pocket_df["system_ID"].apply(lambda x: key_to_component[key].get(x, None))
             max_component = int(pocket_df[col].max()) + 1
             pocket_df.loc[pocket_df[col].isnull(), col] = [int(max_component) + i for i in range(len(pocket_df[pocket_df[col].isnull()]))]
     return pocket_df
@@ -64,17 +72,11 @@ def main():
     parser.add_argument("--output_dir", type=Path, required=True)
     parser.add_argument("--overwrite", action="store_true")
     parser.add_argument("--thresholds", type=float, nargs="+", default=[0.3, 0.5, 0.7, 0.99])
+    parser.add_argument("--num_threads", type=int, default=8)
     args = parser.parse_args()
-    score_dir = Path(args.score_dir)
-    score_files = list(score_dir.iterdir())
-    if args.type == "strong":
-        key_to_components = label_protein_pocket_clusters(score_files, args.score_name, args.thresholds)
-        with open(args.output_dir / f"{args.score_name}__strong.json", "w") as f:
-            json.dump(key_to_components, f)
-    else:
-        key_to_components = label_protein_pocket_clusters(score_files, args.score_name, args.thresholds, strong=False)
-        with open(args.output_dir / f"{args.score_name}__weak.json", "w") as f:
-            json.dump(key_to_components, f)
+    key_to_components = label_protein_pocket_clusters(Path(args.score_dir), args.score_name, args.thresholds, strong=args.type == "strong", num_threads=args.num_threads)
+    with open(args.output_dir / f"{args.score_name}__{args.type}.json", "w") as f:
+        json.dump(key_to_components, f)
 
 if __name__ == "__main__":
     main()
